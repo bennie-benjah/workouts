992419e3be6b05605fb4cc51d365b1ec77062f25
benjamin <mutisob78@gmail.com>
2025-10-30 15:44:22 +0300

    commit4

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
-//Create an Express application
-const app = express()
-//middleware
+
+// Create an Express application
+const app = express();
+
+// Middleware
 app.use(express.json());
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
-//routes
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running successfully 🚀');
+});
+
+// Routes
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/user', userRoutes);
-//connect to the database
+
+// Connect to the database
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  //listen for requests
-  app.listen(process.env.PORT, () => {
-    console.log(`Connected to DB & Server is running on port ${process.env.PORT}`);
+  .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB & Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-})
-.catch((error) => {
-  console.log(error);
-});
-//listen on port 8000
