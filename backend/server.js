e571db178f1d05d786538451fed8b11d66e397e0
benjamin <mutisob78@gmail.com>
2025-10-30 11:47:01 +0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
+//Create an Express application
+const app = express()
+//middleware
+app.use(express.json());
+app.use((req, res, next)=> {
+  console.log(req.path, req.method);
+  next();
+});
+//routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
+//connect to the database
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  //listen for requests
+  app.listen(process.env.PORT, () => {
+    console.log(`Connected to DB & Server is running on port ${process.env.PORT}`);
+  });
+})
+.catch((error) => {
+  console.log(error);
+});
+//listen on port 8000
